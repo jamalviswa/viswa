1dce66cf5061ef8ad1117510bc2133a14a09ac9d
winsvts <jamal.viswatech@gmail.com>
2022-05-27 16:08:00 +0530

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,98 +1,99 @@
- <!-- Footer Start -->
- <div class="container-fluid bg-dark text-light mt-2 wow fadeInUp" data-wow-delay="0.1s">
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-light mt-2 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="row gx-5">
 
-<div class="container">
-    <div class="row gx-5">
-        <div class="col-lg-4 col-md-4 footer-about">
-            <div class="d-flex flex-column align-items-center justify-content-center text-center h-100  p-4" style="background-color: #000 !important;">
-                <a href="index.html" class="navbar-brand">
-                    <h1 class="m-0 text-white"><img src="assets\logo2.gif" width="250" alt="VTS"></h1>
-                    <!--<img src="\assets\logo.png" width="150" height="50"alt="VTS">-->
-                </a>
-                <p class="mt-3 mb-4 text-white">We are a web design and development Company from Coimbatore , Tamilnadu helping enterprise business in their digital transition through our suite of web and mobile application Services.</p>
-                <!--<form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control border-white p-3" placeholder="Your Email">
-                        <button class="btn btn-dark">Sign Up</button>
-                    </div>
-                </form>-->
-            </div>
-        </div>
-        <div class="col-lg-8 col-md-4">
-            <div class="row gx-5">
-               
-                <!--<div class="col-lg-4 col-md-12 pt-5 mb-5">-->
-                <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
-                    <div class="section-title section-title-sm position-relative pb-3 mb-4">
-                        <h3 class="text-light mb-0">Business Hours</h3>
-                    </div>
-                    <div class="link-animated d-flex flex-column justify-content-start">
-                        <h6 class="fw-bold text-primary text-uppercase mb-3"> Opening Days :</h6>
-                        <p class="mb-4"> Monday - Friday : 10 am to 7 pm</p>
-                        <h6 class="fw-bold text-primary text-uppercase">Vacation :</h6>
-                        <p class="mb-4"> All Sundays & National Holidays</p>
-                    </div>
+            <!-- Footer First Part start-->
+            <div class="col-lg-4 col-md-4 footer-about">
+                <div class="d-flex flex-column align-items-center justify-content-center text-center h-100  p-4" style="background-color: #000 !important;">
+                    <a href="index.html" class="navbar-brand">
+                        <h1 class="m-0 text-white"><img src="{{URL::to('images/v_logo.png')}}" width="250" alt="VTS"></h1>
+                    </a>
+                    <p class="mt-3 mb-4 text-white">We are a web design and development Company from Coimbatore,Tamilnadu helping enterprise business in their digital transition through our suite of web and mobile application Services.</p>
                 </div>
-
-
-                <div class="col-lg-3 col-md-12 pt-0 pt-lg-5 mb-5">
-                    <div class="section-title section-title-sm position-relative pb-3 mb-4">
-                        <h3 class="text-light mb-0">Quick Links</h3>
-                    </div>
-                    <div class="link-animated d-flex flex-column justify-content-start">
-                        <a class="text-light mb-2" href="index.html"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
-                        <a class="text-light mb-2" href="about.html"><i class="bi bi-arrow-right text-primary me-2"></i>About</a>
-                        <a class="text-light mb-2" href="service.html"><i class="bi bi-arrow-right text-primary me-2"></i>Services</a>
-                        <a class="text-light mb-2" href="blog.html"><i class="bi bi-arrow-right text-primary me-2"></i>Blogs</a>
-                        <a class="text-light mb-2" href="videos.html"><i class="bi bi-arrow-right text-primary me-2"></i>Videos</a>
-                        <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Products</a>
-                        <a class="text-light" href="contact.html"><i class="bi bi-arrow-right text-primary me-2"></i>Contact</a>
-                    </div>
-                </div>
-                <div class="col-lg-5 col-md-12 pt-5 mb-5">
-                    <div class="section-title section-title-sm position-relative pb-3  mb-4">
-                        <h3 class="text-light mb-0"> Get In Touch</h3>
-                    </div>
-                    <div class="d-flex mb-4">
-                        <i class="bi bi-geo-alt text-primary me-2"></i>
-                        <p class="mb-0">Saibaba Colony,Coimbatore</p>
-                    </div>
-                    <div class="d-flex mb-4">
-                        <i class="bi bi-envelope-open text-primary me-2"></i>
-                        <p class="mb-0">info@viswatechnologysolutions.com</p>
-                    </div>
-                    <div class="d-flex mb-4">
-                        <i class="bi bi-telephone text-primary me-2"></i>
-                        <p class="mb-0">+91 7867997754</p>
-                    </div>
-                    <div class="d-flex mt-4">
-
-                        <a class="btn btn-primary btn-square me-2" href="https://www.facebook.com/viswatechnologysolutions/"><i class="fab fa-facebook-f fw-normal"></i></a>
-                        <a class="btn btn-primary btn-square me-2" href="https://twitter.com/SolutionViswa"><i class="fab fa-twitter fw-normal"></i></a>
-                        <a class="btn btn-primary btn-square me-2" href="https://www.instagram.com/viswa.technologysolutions/"><i class="fab fa-instagram fw-normal"></i></a>
-                        <a class="btn btn-primary btn-square me-2" href="https://www.linkedin.com/in/viswa-technology-solutions-566a77209/"><i class="fab fa-linkedin-in fw-normal"></i></a>
-
-                    </div>
-                </div>
-
             </div>
-        </div>
-    </div>
-</div>
-</div>
-<div class="container-fluid text-white" style="background: #061429;">
-<div class="container text-center">
-    <div class="row justify-content-end">
-        <div class="col-lg-8 col-md-6">
-            <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
-                <p class="mb-0">&copy; <a class="text-white border-bottom" href="#">2022 Viswa Technology Solution</a>. All Rights Reserved.
+            <!-- Footer First Part End-->
 
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/
-                Designed by <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a></p>
-                <br>Distributed By: <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
-            </div>-->
+            <!-- Footer Second Part start-->
+            <div class="col-lg-8 col-md-4">
+                <div class="row gx-5">
+
+                    <!-- Business Hours Start -->
+                    <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+                        <div class="section-title section-title-sm position-relative pb-3 mb-4">
+                            <h3 class="text-light mb-0">Business Hours</h3>
+                        </div>
+                        <div class="link-animated d-flex flex-column justify-content-start">
+                            <h6 class="fw-bold text-primary text-uppercase mb-3"> Opening Days :</h6>
+                            <p class="mb-4"> Monday - Friday : 10am to 6pm</p>
+                            <h6 class="fw-bold text-primary text-uppercase">Vacations :</h6>
+                            <p class="mb-4"> All Sundays & National Holidays</p>
+                        </div>
+                    </div>
+                    <!-- Business Hours End -->
+
+                    <!-- Quick Link Start -->
+                    <div class="col-lg-3 col-md-12 pt-0 pt-lg-5 mb-5">
+                        <div class="section-title section-title-sm position-relative pb-3 mb-4">
+                            <h3 class="text-light mb-0">Quick Links</h3>
+                        </div>
+                        <div class="link-animated d-flex flex-column justify-content-start">
+                            <a class="text-light mb-2" href="{{ url('home') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                            <a class="text-light mb-2" href="{{ url('about') }}"><i class="bi bi-arrow-right text-primary me-2"></i>About</a>
+                            <a class="text-light mb-2" href="{{ url('services') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Services</a>
+                            <a class="text-light mb-2" href="{{ url('blogs') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Blogs</a>
+                            <a class="text-light mb-2" href="{{ url('videos') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Videos</a>
+                            <a class="text-light mb-2" href="{{ url('portfolio') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Portfolio</a>
+                            <a class="text-light" href="{{ url('contact') }}"><i class="bi bi-arrow-right text-primary me-2"></i>Contact</a>
+                        </div>
+                    </div>
+                    <!-- Quick Link End -->
+
+                    <!-- Get in touch start -->
+                    <div class="col-lg-5 col-md-12 pt-5 mb-5">
+                        <div class="section-title section-title-sm position-relative pb-3  mb-4">
+                            <h3 class="text-light mb-0">Get In Touch</h3>
+                        </div>
+                        <div class="d-flex mb-4">
+                            <i class="bi bi-geo-alt text-primary me-2"></i>
+                            <p class="mb-0">83A,Ramalingam Colony,Saibaba Colony,Coimbatore</p>
+                        </div>
+                        <div class="d-flex mb-4">
+                            <i class="bi bi-envelope-open text-primary me-2"></i>
+                            <p class="mb-0">info@viswatechnologysolutions.com</p>
+                        </div>
+                        <div class="d-flex mb-4">
+                            <i class="bi bi-telephone text-primary me-2"></i>
+                            <p class="mb-0">+91 78679 97754</p>
+                        </div>
+                        <div class="d-flex mt-4">
+                            <a class="btn btn-primary btn-square me-2" href="https://www.facebook.com/viswatechnologysolutions/"><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-primary btn-square me-2" href="https://twitter.com/SolutionViswa"><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-primary btn-square me-2" href="https://www.instagram.com/viswa.technologysolutions/"><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-primary btn-square me-2" href="https://www.linkedin.com/in/viswa-technology-solutions-566a77209/"><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </div>
+                    </div>
+                    <!-- Get in touch End -->
+
+                </div>
             </div>
+            <!-- Footer Second Part End-->
+
         </div>
     </div>
 </div>
 <!-- Footer End -->
+
+<!-- Copyright section Start -->
+<div class="container-fluid text-white" style="background: #061429;">
+    <div class="container text-center">
+        <div class="row justify-content-end">
+            <div class="col-lg-8 col-md-6">
+                <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
+                    <p class="mb-0">&copy; <a class="text-white border-bottom" href="#">2022 Viswa Technology Solutions</a>. All Rights Reserved.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright section End -->
