f48fe4e0fcd174b0aaf870769315c605ea6ef4ba
winsvts <jamal.viswatech@gmail.com>
2022-05-28 17:18:52 +0530

    login2

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -21,44 +21,7 @@
 </head>
 
 <body>
-    <!--  Login Page Start-->
-    <div class="container">
-        <div class="d-flex justify-content-center h-100">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Sign In</h3>
-                </div>
-                <div class="card-body">
-                    <form>
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="username">
-                        </div>
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                            </div>
-                            <input type="password" class="form-control" placeholder="password">
-                        </div>
-                        <div class="row align-items-center remember">
-                            <input type="checkbox">Remember Me
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" value="Login" class="btn float-right login_btn">
-                        </div>
-                    </form>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex justify-content-center">
-                        <a href="#">Forgot your password?</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Login Page End-->
+@yield('content')
 </body>
 
 </html>
