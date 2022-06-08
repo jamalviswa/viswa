87206fd01c3689ec3611a578d69bcbb8e51c56f9
jamal <jamal.viswatech@gmail.com>
2022-06-09 00:02:53 +0530

    Update index.blade.php

--- resources/views/sitesettings/index.blade.php
+++ resources/views/sitesettings/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.back')
 @section('content')
+
+<!-- Main Content Start-->
 <div id="main-content">
+
+    <!-- Sub Header Section Start-->
     <div class="block-header">
         <div class="row clearfix">
             <div class="col-md-6 col-sm-12">
@@ -11,10 +15,12 @@
                     <li class="breadcrumb-item"><a href="#"><i class="icon-home"></i></a></li>
                     <li class="breadcrumb-item active">Site Settings</li>
                 </ul>
-
             </div>
         </div>
     </div>
+    <!-- Sub Header Section Start-->
+
+    <!-- Edit Sitesettings Section Start-->
     <div class="container-fluid">
         <div class="row clearfix">
             <div class="col-md-12">
@@ -23,122 +29,154 @@
                         <h2>Edit Site Settings</h2>
                     </div>
                     <div class="body">
-                        <div class="row clearfix">
-                            <div class="col-lg-12 col-md-12 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Site Title</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-12 col-md-12 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Meta Description</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-12 col-md-12 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Meta Keywords</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Logo</label>
-                                    <div class="custom-file">
-
-                                        <input type="file" class="form-control custom-file-input" id="inputGroupFile01">
-                                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                        <form method="post">
+                            <div class="row clearfix">
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Site Title</label>
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group m-t-20">
-                                    <img src="{{URL::to('images/v_logo.png')}}" width="150" height="50" alt="VTS">
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">FavIcon</label>
-                                    <div class="custom-file">
-
-                                        <input type="file" class="form-control custom-file-input" id="inputGroupFile01">
-                                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Meta Description</label>
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group m-t-20">
-                                    <img src="{{URL::to('images/v_favicon.png')}}" width="48" height="48" alt="VTS">
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Meta Keywords</label>
+                                        <input type="text" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="header">
-                        <h2>Edit Contact Information</h2>
-                    </div>
-                    <div class="body">
-                        <div class="row clearfix">
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Mobile Phone Number</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fa fa-mobile-phone"></i></span>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Logo</label>
+                                        <div class="custom-file">
+
+                                            <input type="file" class="form-control custom-file-input" id="inputGroupFile01">
+                                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
                                         </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group m-t-20">
+                                        <img src="{{URL::to('images/v_logo.png')}}" width="150" height="50" alt="VTS">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">FavIcon</label>
+                                        <div class="custom-file">
+
+                                            <input type="file" class="form-control custom-file-input" id="inputGroupFile01">
+                                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group m-t-20">
+                                        <img src="{{URL::to('images/v_favicon.png')}}" width="48" height="48" alt="VTS">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Mobile Phone Number</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-mobile-phone"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">TelePhone Number</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-phone"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Email</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-envelope-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Alternate Email</label>
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-envelope-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Address</label>
+                                        <textarea class="form-control" rows="5" cols="30"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Map Url</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Facebook Url</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Whatsapp Url</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">LinkedIn Url</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Instagram Url</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label class="control-label">Twitter Url</label>
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">TelePhone Number</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fa fa-phone"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Email</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fa fa-envelope-o"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Alternate Email</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fa fa-envelope-o"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <div class="form-group">
-                                    <label class="control-label">Address</label>
-                                    <textarea class="form-control" rows="5" cols="30"></textarea>
-                                </div>
-                            </div>
-                           
-
-
-                        </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                            <button type="submit" class="btn btn-danger">Reset</button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+     <!-- Edit Sitesettings Section End-->
+
 </div>
-</div>
+<!-- Main Content End-->
 @endsection
